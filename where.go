315ef4af7e3049d94fb3eefc048bd03c708d1b78
root <lj@vipex.cc>
2021-09-01 10:25:00 +0800

    优化代码

--- where.go
+++ where.go
@@ -3,10 +3,8 @@ package linq
 func (q *query[T]) Where(f func(T) bool) *query[T] {
 	var list = new(List[T])
 	for _, v := range *q.Val {
-		// for _, v := range q.ToArray() {
 		if f(v) {
 			*list = append(*list, v)
-			// q = q.Add(v)
 		}
 	}
 	q.Val = list
